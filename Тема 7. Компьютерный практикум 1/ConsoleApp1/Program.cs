fd23013fe14fe7af69e3b5c16480a0e1f7134e22
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-10-15 23:56:28 +0300

    Add files via upload

---
+++ Тема 7. Компьютерный практикум 1/ConsoleApp1/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Тема 7
+
+            // Задача 1.Соответствие номера из таблицы Unicode символу
+
+            //Console.Title = "Программа №1(Цвигуненко)";
+
+            //for (int i = 0; i <= 255; i++)
+
+            //{
+
+            //    Console.WriteLine($"Код {i} соотв.символу {(char)i}");   
+
+            //}
+
+
+
+            //Задача 2.Дан символ.Вывести его код
+
+            //Console.Title = "Программа №2(Цвигуненко)";
+
+            //Console.Write("Введите символ:");
+
+            //char ch = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Символ {ch} имеет код {(int)ch}");
+
+
+
+            //Задача 3.Дан символ.Вывести символ, который в кодовой таблице ASCII следует за этим символом
+
+            //Console.Title = "Программа №3(Цвигуненко)";
+
+            //Console.Write("Введите символ:");
+
+            //char ch = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"символ{(char)(int)ch + 1}");
+
+
+
+            //Задача 4.Дан символ.Вывести два символа, стоящие перед ним в кодовой таблице ASCII
+
+            //Console.Title = "Программа №4(Цвигуненко)";
+
+            //Console.Write("Введите символ:");
+
+            //char a = Convert.ToChar(Console.ReadLine());
+
+            //int b = (int)a;
+
+            //b -= 1;
+
+            //int z = (int)a;
+
+            //z -= 2;
+
+            //Console.WriteLine($"За символом {a} символ {(char)b} и {(char)z} ");
+
+            //Console.ReadKey();
+
+
+
+            //Задача 5.Дан символ.Вывести два символа, первый из которых предшествует введенному символу в кодовой таблице, а второй – следует после него
+
+            //Console.Title = "Программа №5(Цвигуненко)";
+
+            //Console.Write("Введите символ:");
+
+            //char d = Convert.ToChar(Console.ReadLine());
+
+            //int b = (int)d;
+
+            //b -= 1;
+
+            //int z = (int)d;
+
+            //z += 1;
+
+            //Console.WriteLine($"Перед симвоолом {d} символ {(char)b} и После {(char)z} ");
+
+            //Console.ReadKey();
+
+
+
+            //Задача 6.Дано название футбольного клуба.Напечатать его на экране «столбиком»
+
+            //Console.Title = "Программа №6(Цвигуненко)";
+
+            //Console.Write("Введите название клуба:");
+
+            //string ch = (Console.ReadLine());
+
+            //char[] omas = ch.ToCharArray();
+
+            //for (int i = 0; i < omas.Length; i++)
+
+            //{
+
+            //    Console.WriteLine(omas[i]);
+
+            //}
+
+
+
+            // Задача 7.Составить программу, которая печатает заданное слово, начиная с последней буквы
+
+            //Console.Title = "Программа №7(Цвигуненко)";
+
+            //Console.Write("Введите слово:");
+
+            //string ch = (Console.ReadLine());
+
+            //char[] omas = ch.ToCharArray();
+
+            //for (int i  = omas.Length-1; i >=0; i--)
+
+            //{
+
+            //    Console.Write(omas[i]);
+
+            //}
+
+
+
+
+
+            //Задача 8.Преобразовать символы в строке из строчных в прописные и обратно
+
+            //Console.Title = "Программа №8(Цвигуненко)";
+
+            //Console.Write("Введите строку:");
+
+            //string ch = Console.ReadLine();
+
+            //char [] omas = ch.ToCharArray();
+
+            //for (int i = 0; i < omas.Length; i++)
+
+            //{
+
+            //    Console.Write(char.ToUpper(omas[i]));
+
+            //}
+
+            //Console.WriteLine();
+
+            //for (int i = 0; i < omas.Length; i++)
+
+            //{
+
+            //    Console.Write(char.ToLower(omas[i]));
+
+            //}
+
+            //    Console.ReadKey();
+
+
+        }
+    }
+}
